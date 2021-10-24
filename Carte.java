195b64c8067a19c42f4369572dc3c52bffbab811
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2021-10-24 05:50:45 +0200

    Create Carte.java

---
+++ Carte.java
@@ -0,0 +1,32 @@
+package projet;
+
+import java.util.Objects;
+
+
+public record Carte(String couleur, int coût, int points) {
+	
+	
+	/**
+	 * Constructeur du type Carte
+	 * 
+	 * @param couleur
+	 * @param coût
+	 * @param points
+	 */
+	public Carte{
+		
+		/*Verifier avec switchs toutes les couleurs*/
+		
+		Objects.requireNonNull(couleur, "Carte de couleur null");
+		
+		if(coût < 0) {
+			throw new IllegalArgumentException("Coût négatif de la carte");
+		}
+		
+		if(points < 0) {
+			throw new IllegalArgumentException("Points de prestiges négatifs de la carte");
+		}
+		
+	}
+	
+}
