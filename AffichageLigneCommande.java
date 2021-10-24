f6ba158ea42a18df0e1394e33c4d5bae694e80be
DDJM <79527494+DDJM-77@users.noreply.github.com>
2021-10-24 14:56:57 +0200

    Add files via upload

---
+++ AffichageLigneCommande.java
@@ -0,0 +1,24 @@
+
+public class AffichageLigneCommande {
+	
+	public static void showPlateau(Partie game) {
+		showJeton(game);
+		showBoard(game);
+		for(var joueur : game.joueurs) {
+			showRessourceJoueur(joueur);
+		}
+		
+	}
+	
+	public static void showJeton(Partie game) {
+		//
+	}
+	
+	public static void showBoard(Partie game) {
+		//
+	}
+	
+	public static void showRessourceJoueur(Joueur joueur) {
+		//
+	}
+}
