203e7db366287df98a69d89d763f184f7f558d9d
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2021-12-12 20:07:51 +0100

    Update AffichageLigneCommande.java

--- AffichageLigneCommande.java
+++ AffichageLigneCommande.java
@@ -5,7 +5,7 @@ import java.lang.StringBuilder;
 public class AffichageLigneCommande {
 	
 	/**
-	 * Affiche le plateau de jeu.
+	 * Print the 
 	 * 
 	 * @param game
 	 */
@@ -18,12 +18,12 @@ public class AffichageLigneCommande {
 		
 		showBoard(game);
 		
-		showJeton(game.jetons_disponibles, true);
+		showJeton(game.jetons_disponibles(), "JETON");
 		
 		
 		System.out.println("\n\n========================================== JOUEURS ==========================================\n\n");
 		
-		for(var joueur : game.joueurs) {
+		for(var joueur : game.joueurs()) {
 			showJoueur(joueur);
 			System.out.println("\n");
 		}
@@ -35,8 +35,9 @@ public class AffichageLigneCommande {
 	 * 
 	 * @param ressources
 	 */
-	public static void showJeton(HashMap<String, Integer> ressources, boolean message) {
+	public static void showJeton(HashMap<String, Integer> ressources, String message) {
 		
+		StringBuilder chaine_tab = new StringBuilder();
 		StringBuilder chaine_noms = new StringBuilder();
 		StringBuilder chaine_quantite = new StringBuilder();
 		String separator = "|    ";
@@ -44,8 +45,8 @@ public class AffichageLigneCommande {
 		
 		
 		
-		if(message) {
-			System.out.println("\n\n========================================== JETONS ==========================================\n\n");
+		if(message != null) {
+			System.out.println("\n\n========================================== "+ message + " ==========================================\n\n");
 		}
 		
 		for(var entry : ressources.entrySet()) {
@@ -63,178 +64,102 @@ public class AffichageLigneCommande {
 			 
 			 separator = "    |    ";
 			 separator_quantite = "      |      ";
-			 
-			 
-			//System.out.println(" ―――――――――――――\n|    "+ jeton_name +"    |\n ―――――――――――――\n|      " + jeton_quantite + "      |\n ―――――――――――――\n\n");
 		}
 		
 		chaine_noms.append("    |");
 		chaine_quantite.append("      |");
-		
-		
-		System.out.println(" ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ―――――――――――――");
+		for(var i = 0; i< ressources.size();i++) {
+			chaine_tab.append(" ―――――――――――――");
+		}
+		System.out.println(chaine_tab);
 		System.out.println(chaine_noms);
-		System.out.println(" ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ―――――――――――――");
+		System.out.println(chaine_tab);
 		System.out.println(chaine_quantite);
-		System.out.println(" ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ――――――――――――― ―――――――――――――");
-		
+		System.out.println(chaine_tab);
+
 	}
+
 	
 	/**
-	 * Affiche les cartes constituant le board.
+	 * Print the cards of the board of the game token.
 	 * 
 	 * @param game
+	 *        Game token to print it board
 	 */
 	public static void showBoard(Partie game) {
 		
-	
-		
-		
-		for(int i = 1 ; i < game.board.size() + 1 ; i++) {
+		for(int i = 1 ; i < game.board().size() + 1 ; i++) {
 			
-			if(game.board.get(i).get(0) != null) {
+			var num_card = 1;
+			
+			if(game.board().get(i).get(0) != null) {
 				
-				System.out.println("       NIVEAU " + i);
+				System.out.println("\n\n    -- NIVEAU " + i + " --\n\n");
 				
-				for(var carte : game.board.get(i)) {
+				for(var carte : game.board().get(i)) {
+					
+					System.out.println("\n      Carte n° " + num_card + "\n");
+					
 					if(carte != null)
 						System.out.println(carte);
+					num_card ++;
+					
 				}
 			}
 			
 		}
-		
-		
-	
-		
-		
-//		StringBuilder first_line = new StringBuilder();
-//		StringBuilder second_line = new StringBuilder();
-//		StringBuilder third_line = new StringBuilder();
-//		StringBuilder fourth_line = new StringBuilder();
-//		StringBuilder fifth_line = new StringBuilder();
-//		
-//		String separator = "|  ";
-//		String second_separator = "|  ";
-//		
-//		
-//		System.out.println("\n\n========================================== BOARD ==========================================\n\n");
-//		
-//		/*FORMATER LA CHAINE DE FAÇON A CE QU'ELL AIT LA MÊME LONGUEUR QUEL QUE SOIT LE NOM DE LA COULEUR*/
-//		for(var carte : game.board.get(1)) {
-//			
-//			
-//			var card_name = carte.couleur();
-//			
-//			
-//			if(carte.couleur().equals("Vert") || carte.couleur().equals("Noir") || carte.couleur().equals("Bleu")) {
-//				card_name = card_name + " ";
-//			}
-//			
-//			
-//			first_line.append(separator).append(carte.points()).append("          ").append(card_name);
-//			
-//			int r = 1;
-//			
-//			for(var couleur : carte.coût().entrySet()) {
-//				
-//				var couleur_name = couleur.getKey();
-//				var couleur_value = couleur.getValue();
-//				
-//				switch(r) {
-//					case 1 :{
-//						second_line.append(second_separator).append(couleur_name).append(": ").append(couleur_value);
-//						break;
-//					}
-//					case 2 :{
-//						third_line.append(second_separator).append(couleur_name).append(": ").append(couleur_value);
-//						break;
-//					}
-//					case 3 :{
-//						fourth_line.append(second_separator).append(couleur_name).append(": ").append(couleur_value);
-//						break;
-//					}
-//					case 4 :{
-//						fifth_line.append(second_separator).append(couleur_name).append(": ").append(couleur_value);
-//						break;
-//					}
-//				};
-//				
-//				r ++;
-//			}
-//			
-//
-//			
-//			
-//			
-//			separator = "  | |  ";
-//			second_separator = "          | |  ";
-//			
-//			//System.out.println("      CARTE : "+ i +"\n\n ――――――――――――――――――――\n|                    |\n|  " + carte.points() + "          "+ couleur_name + "  |\n|                    |\n|                    |\n|                    |\n|                    |\n|  " + couleur_name + ": " + carte.coût() + "          |\n|                    |\n ――――――――――――――――――――\n\n");
-//		}
-//		
-//		first_line.append("  |");
-//		second_line.append("          |");
-//		third_line.append("          |");
-//		fourth_line.append("          |");
-//		fifth_line.append("          |");
-//		
-//		
-//		System.out.println("        Carte 1                Carte 2                Carte 3                Carte 4      \n");
-//		System.out.println(" ――――――――――――――――――――   ――――――――――――――――――――   ――――――――――――――――――――   ――――――――――――――――――――");
-//		
-//		System.out.println("|                    | |                    | |                    | |                    |");
-//		
-//		System.out.println(first_line);
-//		
-//		System.out.println("|                    | |                    | |                    | |                    |");
-//		System.out.println("|                    | |                    | |                    | |                    |");
-//		System.out.println("|                    | |                    | |                    | |                    |");
-//		System.out.println("|                    | |                    | |                    | |                    |");
-//		
-//		System.out.println(second_line);
-//		System.out.println(third_line);
-//		System.out.println(fourth_line);
-//		System.out.println(fifth_line);
-//		
-//		System.out.println(" ――――――――――――――――――――   ――――――――――――――――――――   ――――――――――――――――――――   ――――――――――――――――――――");
-		
 	}
 	
 	/**
-	 * Affiche les information d'un joueur
+	 * Print the player informations.
 	 * 
 	 * @param joueur
 	 */
 	public static void showJoueur(Joueur joueur) {
 		
 		
-		System.out.println("Joueur : " + joueur.pseudo + "\n\nPoints de prestiges : " + joueur.points_prestiges + "\n\nRessources : \n");
-		
-		showJeton(joueur.ressources, false);
+		System.out.println("Joueur : " + joueur.pseudo() + "\n\nPoints de prestiges : " + joueur.points_prestiges() + "\n\nRessources : \n");
+		System.out.println("Joueur :  " + "\n");
+		showJeton(joueur.ressources(), "JETON");
+		showJeton(joueur.bonus(), "BONUS");
 		
 		
 	}
 	
-	
+	/**
+	 * Print the 
+	 * 
+	 * @param game
+	 */
 	public static void showTuiles(Partie game){
 		
-		for(int i = 0 ; i < game.tuiles_board.size(); i++) {
-			
-			System.out.println(game.tuiles_board.get(i));
-				
+		System.out.println("    -- NOBLES --   \n\n");
 		
+		for(int i = 0 ; i < game.tuiles_board().size(); i++) {
 			
+			System.out.println(game.tuiles_board().get(i));
 		}
 	}
 	
 	
-	
+	/**
+	 * Print the cards reserved by a player.
+	 * 
+	 * @param joueur
+	 * 		  Player of the
+	 */
 	public static void showReserved(Joueur joueur) {
+		
+		if(joueur.reserve().size() == 0) {
+			System.out.println("Vous ne possèdez aucune carte réservée \n\n");
+			return;
+		}
 		
 		System.out.println("Cartes que vous avez réservé \n\n");
 		
-		for(var elem : joueur.reserve) {
+		
+		
+		for(var elem : joueur.reserve()) {
 			System.out.println(elem + "\n");
 		}
 	}
