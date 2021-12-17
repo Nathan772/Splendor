4b2c2eb32aaa0890813ab029d09a332df39591a0
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2021-12-17 14:59:44 +0100

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -5,25 +5,49 @@ import java.util.List;
 
 
 /**
+ * Declaration of the type Joueur. It is a player who belongs to a Splendor game.
  * 
- * @author dylandejesus
- *
+ * @author dylandejesus nathanbilingi
  */
 public class Joueur {
 	
 	/**
-	 * Champs constituant la classe Joueur
+	 * Number of cards earned by a player
 	 */
 	private int cartes;
+	
+	/**
+	 * Name of the player during the game
+	 */
 	private final String pseudo;
+	
+	/**
+	 * Age of the player
+	 */
 	private final int age;
+	
+	/**
+	 * Number of points value
+	 */
 	private int points_prestiges;
+	
+	/**
+	 * Player's resources (tokesn he has)
+	 */
 	private HashMap<String, Integer> ressources;
+	
+	/**
+	 * Player's bonus (when he pays developments cards)
+	 */
 	private HashMap<String, Integer> bonus;
+	
+	/**
+	 * All the reserved cards of a player
+	 */
 	private ArrayList<CarteDev> reserve;
 
 	/**
-	 * Constructor of the type Joueur
+	 * Constructor of the type Joueur.
 	 * 
 	 * @param pseudo
 	 * 		 Player name
@@ -55,11 +79,7 @@ public class Joueur {
 		this.ressources = new HashMap<>();
 		this.initRessourcesMap();
 		this.initBonus();
-		
-		
 	}
-	
-	
 	
 	/**
 	 * Constructor of the type Joueur. Is called if we create a player without giving any Points value.
@@ -75,42 +95,68 @@ public class Joueur {
 		this(pseudo, age, 0);
 	}
 	
-	
-	
-	
-	
+	/**
+	 * cartes field getter.
+	 * 
+	 * @return cartes field value
+	 */
 	public int cartes() {
 		return this.cartes;
 	}
 	
+	/**
+	 * pseudo field getter.
+	 * 
+	 * @return pseudo field value
+	 */
 	public String pseudo() {
 		return this.pseudo;
 	}
 	
-	
+	/**
+	 * age field getter.
+	 * 
+	 * @return age field value
+	 */
 	public int age() {
 		return this.age;
 	}
 	
-	
+	/**
+	 * points_prestiges field getter.
+	 * 
+	 * @return points_prestiges field value
+	 */
 	public int points_prestiges() {
 		return this.points_prestiges;
 	}
 	
+	/**
+	 * ressources field getter.
+	 * 
+	 * @return ressources field value
+	 */
 	public HashMap<String, Integer> ressources() {
 		return this.ressources;
 	}
 	
+	/**
+	 * bonus field getter.
+	 * 
+	 * @return bonus field value
+	 */
 	public HashMap<String, Integer> bonus() {
 		return this.bonus;
 	}
 	
-	
+	/**
+	 * reserve field getter.
+	 * 
+	 * @return reserve field value
+	 */
 	public ArrayList<CarteDev> reserve(){
 		return this.reserve;
 	}
-	
-	
 	
 	/**
 	 * Initialize all the coloured tokens bonus (earned by the player during the game). All the tokens start with the value 0. 
@@ -123,7 +169,6 @@ public class Joueur {
 		}	
 	}
 	
-
 	/**
 	 * Initialize all the couloured tokens (those which represent the ressources of the player). All the tokens start with value 0.
 	 */
@@ -143,20 +188,21 @@ public class Joueur {
 		return "Joueur [pseudo= "+ this.pseudo + ", age= " + this.age + ", prestige= " + points_prestiges + ", ressources=" + this.ressources +"]"; 
 	}
 
-	
-	
 	/**
-	 * Remove 
+	 * Remove a player ressources. It removes the ressources thanks to the value given and give it to the bank of tokens. 
 	 * 
 	 * @param type_ressource
-	 *        Nom d'une des couleurs représentant une ressource du jeu
+	 *        Name of one of the colors represented in the game
 	 *        
 	 * @param val
-	 *        Quantite de la ressource a enlever
+	 *        Number of tokens to remove
 	 * 
-	 * @return Nouvelle quantité de la ressource restante
+	 * @return New value of tokens the player have
 	 */
 	public int enleveRessource(String type_ressource, int val, HashMap<String, Integer> ressources_banque) {
+		
+		Objects.requireNonNull(type_ressource);
+		Objects.requireNonNull(ressources_banque);
 		
 		int old_val = this.ressources.get(type_ressource);	//Ressources of the player
 		
@@ -168,8 +214,14 @@ public class Joueur {
 			val = val - this.bonus.get(type_ressource);
 		}
 		
-		
 		if(old_val < val) {
+			
+			/* à essayer si le code ne marche pas*/
+			/*int nouv_val;
+			nouv_val = this.enleveRessource(name, val, game.jetons_disponibles());
+			this.ressources.put(name, nouv_val);
+			
+			game.jetons_disponibles().put(name,  game.jetons_disponibles().get(name) + nouv_val);	*/
 			
 			ressources_banque.put("Jaune", ressources_banque.get("Jaune") + ((-1)*(old_val - val)));	//Jokers rendus à la banque
 			this.ressources.put("Jaune", this.ressources.get("Jaune") - ((-1)*(old_val - val)));	//Joker retirés au joueur
@@ -181,13 +233,13 @@ public class Joueur {
 	
 	
 	/**
-	 * Réécriture de la fonction equals, permet de savoir si deux Joueurs sont egaux ou non. Renvoie
-	 * true si deux joueurs sont egaux et false sinon.
+	 * Rewrite of the equals function, allows to know if two Players are equal or not. Returns
+	 * true if two players are equal and false otherwise.
 	 * 
 	 * @param player
-	 *        Joueur à comparer.
+	 *        Second player to compare to the other.
 	 *        
-	 * @return True si les deux joueurs sont les mêmes et false sinon.
+	 * @return True if the two players are equals and false otherwise
 	 */
 	@Override
 	public boolean equals(Object player) {
@@ -199,49 +251,57 @@ public class Joueur {
 	
 	
 	/**
-	 * Ajoute le bonus du jeton donnée au joueur.
+	 * Add the bonus token given ti the player.
 	 * 
 	 * @param jeton_bonus
-	 *        Jeton considéré comme un bonus à ajouter.
+	 *        Token considered like the bonus to add
 	 */
 	private void addBonus(Jeton jeton_bonus) {
+		
+		Objects.requireNonNull(jeton_bonus);
 		
 		this.bonus.put(jeton_bonus.couleur(), this.bonus.get(jeton_bonus.couleur()) + 1);
 	}
 	
-	
-	
 	/**
-	 * Ajout d'une quantité définie d'une ressource du joueur.
+	 * Adding a defined amount of a player's resource.
 	 * 
 	 * @param type_ressource
-	 *        Nom d'une des couleurs représentant une ressource du jeu
+	 *        Name of one of the colors représenting a resource of the game.
 	 *        
 	 * @param val
-	 * 		  Quantite de la ressource a enlever
+	 * 		  Number of tokens to remove
 	 * 
-	 * @return Nouvelle quantité de la ressource ajoutée
+	 * @return New number of tokens the player has
 	 */
 	public int ajouteRessource(String type_ressource, int val) {
+		
+		Objects.requireNonNull(type_ressource);
+		
 		return this.ressources.get(type_ressource) + val;
 	}
 	
 	
 	/**
-	 * Achat d'une carte du jeu par le joueur. La carte est acheté si la ressource 
-	 * demandée est possèdée en quantité suffisante par le joueur.
+	 * The player buys a card from the game. The card is bought if the resource
+	 * requested is possessed in sufficient quantity by the player.
 	 * 
 	 * @param carte
-	 *        Carte à acheter
+	 *        Card to earn
+	 *        
+	 * @param game
+	 *        Game in which buy the card
 	 * 
-	 * @return true si la carte a bien pu être achetée et false sinon
+	 * @return True if the card has successfully been earned and false otherwise.
 	 */
 	public boolean acheteCarte(CarteDev carte, Partie game) {
+		
+		Objects.requireNonNull(carte);
+		Objects.requireNonNull(game);
 		
 		if(!this.checkMoney(carte, game)) {
 			return false;
 		}
-		
 		
 		this.addPrestige(carte.points());
 		
@@ -255,8 +315,7 @@ public class Joueur {
 			nouv_val = this.enleveRessource(name, val, game.jetons_disponibles());
 			this.ressources.put(name, nouv_val);
 			
-			game.jetons_disponibles().put(name,  game.jetons_disponibles().get(name) + nouv_val);
-			
+			game.jetons_disponibles().put(name,  game.jetons_disponibles().get(name) + nouv_val);	
 		}
 		
 		this.addBonus(new Jeton(carte.couleur()));
@@ -267,15 +326,22 @@ public class Joueur {
 	
 	
 	/**
+	 * Returns true if a Noble is visiting the player and false otherwise. A Noble is visiting a player if
+	 * he possesses the same bonus as the Noble cost. Many nobles can visit a player
 	 * 
 	 * @param nobles_visiting
+	 *        List of nobles who visit the player
+	 *        
 	 * @param tuiles_board
-	 * @return
+	 *        Nobles on the board
+	 * 
+	 * @return Returns true if a Noble is visiting the player and false otherwise
 	 */
 	public boolean isNobleVisiting(ArrayList<Tuile> nobles_visiting, ArrayList<Tuile> tuiles_board){
+
+		Objects.requireNonNull(tuiles_board);
 		
 		for(var noble : tuiles_board) {
-			
 			
 			var is_visiting = true;
 			
@@ -290,8 +356,7 @@ public class Joueur {
 			
 			if(is_visiting) {
 				nobles_visiting.add(noble);
-			}
-			
+			}	
 		}
 		
 		if(nobles_visiting.size() != 0) {
@@ -302,13 +367,18 @@ public class Joueur {
 	
 	
 	/**
-	 * 
+	 * Check if the player has enough tokens in his resources to pay the card given.
+	 *
 	 * @param card
-	 * @return
+	 *        Card to pay
+	 *        
+	 * @return Returns ture if the card can be earned or false.
 	 */
-
-		/* dernière version de checkMoney*/
-		private boolean checkMoney(CarteDev card, Partie game) {
+	
+	private boolean checkMoney(CarteDev card, Partie game) {
+		
+		Objects.requireNonNull(card);
+		Objects.requireNonNull(game);
 		
 		var val_joker = this.ressources().get("Jaune");
 		
@@ -332,28 +402,12 @@ public class Joueur {
 		return true;
 	}
 	
-	/* version sans le joker*/
-	
-	private boolean checkMoney_simple(CarteDev card, Partie game) {
-		
-		for(var elem : card.coût().entrySet()) {
-			
-			var name = elem.getKey();
-			var val = elem.getValue();
-			
-			if(this.ressources.get(name) < val) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
 	
 	/**
-	 * Ajout d'une quantite définie de points de prestiges au joueur
+	 * Addin of some points values.
 	 * 
-	 * @param 
-	 *        Quantite de points de prestiges
+	 * @param val
+	 *        Points value to add
 	 */
 	public void addPrestige(int val) {
 		int result = this.points_prestiges + val;
@@ -366,15 +420,17 @@ public class Joueur {
 	}
 
 	/**
-	 * Ajout d'une quantite définie d'une ressource.
+	 * Adding of an amount of a token.
 	 * 
 	 * @param jeton
-	 * 		  Jeton représentant la ressource
-	 * 
+	 *        Token type to add
+	 *        
 	 * @param quantite
-	 *        Quantite de ressource à ajouter
+	 *        	Number of tokens to add
 	 */
 	public void addRessource(Jeton jeton, int quantite) {
+		
+		Objects.requireNonNull(jeton);
 		
 		int quantite_total = this.ressources.get(jeton.couleur()) + quantite;
 		
@@ -383,9 +439,9 @@ public class Joueur {
 	
 	
 	/**
-	 * Vérifie que le nombre de jetons d'un joueur ne dépasse pas 10.
+	 * Check if the number of tokens of a player is under 10.
 	 * 
-	 * @return True si l'utilisateur possèdent 10 jetons ou moins et false sinon
+	 * @return True if the user has 10 tokens or under and false otherwise.
 	 */
 	public boolean checkNbJetons() {
 		
@@ -401,29 +457,49 @@ public class Joueur {
 		
 		return true;
 	}
+	
+	/**
+	 * Check give the number of token that the player have to lose
+	 * 
+	 * @return return the number of tokens that the player have to loose
+	 */
+	public int NbJetons_loose() {
+		
+		int count = 0;
+		
+		for(var nb_jet : this.ressources.values()) {
+			count = count + nb_jet;
+		}
+		
+		return count - 10;
+	}
+
 
 	
 	/**
+	 * Makes a card reservation for the player.
 	 * 
 	 * @param carte
+	 *        card to reserve
+	 *        
 	 * @param ressources_jeu
-	 * @return
+	 *        Tokens available in the game (bank)
+	 *        
+	 * @return True if the reservation is possible (under 3 reserved cards) and false otherwise.
 	 */
 	public boolean reserveCarte(CarteDev carte, HashMap<String, Integer> ressources_jeu) {
 		
-		Objects.requireNonNull(carte, "Card given to keep is null");
+		Objects.requireNonNull(carte);
 		
 		if(this.reserve.size() == 3) {
+			System.out.println("Désolé il n'est pas possible de réserver plus de 3 cartes !");
 			return false;
 		}
 		
 		this.reserve.add(carte);
 		
 		
-		if(ressources.get("Jaune") > 0) {
-			
-			this.addRessource(new Jeton("Jaune"), 1);			
-		}
+		
 		
 		return true;
 	}
@@ -431,7 +507,10 @@ public class Joueur {
 	
 	
 	/**
-	 * Ensemble des tests de méthodes pour le type Joueur
+	 * All the test in relation with the type Joueur.
+	 * 
+	 * @param args
+	 *        All the arguments given in the console
 	 */
 	public static void main(String[] args) {
 		
@@ -453,6 +532,9 @@ public class Joueur {
 	}
 
 }
+
+
+
 
 
 
