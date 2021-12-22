7c153d6c471ee05f291b294aad152abeca2260e2
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2021-12-22 19:09:05 +0100

    Add files via upload

---
+++ Affichage.java
@@ -0,0 +1,14 @@
+
+/**
+ * Interface représentant la manière d'affichage. On y trouve
+ * les affciahges en ligne de commande ainsi que l'affichage graphique 
+ * 
+ * @author dylandejesus
+ */
+public interface Affichage {
+
+	public void showPlateau(Mode game, int mode);
+	public void showBoard(Mode game);
+	public void showReserved(Joueur joueur);
+	public int showJoueur(Joueur joueur);
+}
