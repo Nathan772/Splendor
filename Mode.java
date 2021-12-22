791c15a4c2f2a285e302db87c7d6583328858b3e
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2021-12-22 19:00:48 +0100

    Add files via upload

---
+++ Mode.java
@@ -0,0 +1,461 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+
+
+
+
+/**
+ * 
+ * 
+ * 
+ * FAIRE UNE FONCTION endOfTurn() regroupant toutes les actions de fin dr tour, les nobles, le
+ * 
+ * 
+ * 
+ * @author dylandejesus
+ *
+ */
+public interface Mode {
+
+	/**
+	 * Number of victory points
+	 */
+	static final int VICTORY_POINTS = 15;
+	
+	public ArrayList<Joueur> joueurs();
+	
+	public HashMap<Integer, List<CarteDev>> board();
+	
+	public HashMap <Integer, List<CarteDev>> pioche();
+	
+	public ArrayList<Tuile> tuiles_board();
+	
+	public HashMap<String, Integer> jetons_disponibles();
+	
+	public int taille_pioche();
+	
+	
+	void initialisePartie(int mode);
+	
+	int achatCarte(Joueur joueur, int mode_jeu, Affichage affiche);
+	
+	int reservationCarte(Joueur joueur);
+	
+	int giveNbPlayersPossible();
+	
+	
+	
+	/**
+	 * Add a player in the game list of players
+	 * 
+	 * @param  player
+	 * 		   Player to add
+	 */
+	public default void addPlayer(Joueur player) {
+		this.joueurs().add(player);
+	}
+	
+	
+	/**
+	 * Draw a card from the deck and add it to the board. If you choose to draw a card
+	 * remove the card from the board and add the top card of the deck in its place
+	 * 
+	 * @param  index_supp
+	 * 		   Index of the card ti replace
+	 */
+	public default CarteDev piocheOneCard(int ligne, int index_supp) {
+		
+		int derniere_carte = this.pioche().get(ligne).size() - 1;	
+
+		CarteDev card_picked = this.pioche().get(ligne).remove(derniere_carte);
+
+		
+		this.board().get(ligne).set(index_supp, card_picked);
+		
+		return card_picked;
+	}
+	
+	
+	/**
+	 * Draw 4 cards from the deck.
+	 */
+	default void piocheFourCards(int ligne) {
+		
+		for(int i  =0; i < 4 ;i++) {
+			
+			this.piocheOneCard(ligne, i);
+		}
+	}
+	
+	
+	/**
+	 * Find the player with the most prestige points in the player list.
+	 * If two players have the same number of prestige points, the one with the
+	 * the less development cards. The Method returns the object of type Player.
+	 * 
+	 * @param  classement
+	 * 		   List of players
+	 */
+	private static Joueur topClassement(ArrayList<Joueur> classement) {
+		
+		Objects.requireNonNull(classement);
+		
+		Joueur best_player = classement.get(0);
+		int i = 0;
+		
+		while(classement.get(i).points_prestiges() == best_player.points_prestiges() && i < classement.size() - 1) {
+			
+			if(classement.get(i).cartes() < best_player.cartes()) {
+				best_player = classement.get(i);
+			}
+			
+			i++;
+		}
+		
+		return best_player;
+	}
+	
+	
+	/**
+	 * Exchange two values from the list of players. We exchange the two values located
+	 * index1' and 'index2'.
+	 * 
+	 * @param  index1
+	 *         First index
+	 * 
+	 * @param  index2
+	 * 	       Second index
+	 */
+	private static void swap(ArrayList<Joueur> list, int index1, int index2) {
+		
+		Joueur tmp = list.get(index2);
+
+        list.set(index1, list.get(index2));
+        list.set(index2, tmp);
+	}
+	
+	/**
+	 * Returns true if j1 has less prestige points than j2
+	 * 
+	 * @param  j1
+	 * 		   First player
+	 * 
+	 * @param  j2
+	 *         Second player
+	 */
+	private static boolean compareJoueur(Joueur j1, Joueur j2){
+		
+		Objects.requireNonNull(j1);
+		Objects.requireNonNull(j2);
+		
+		if(j1.points_prestiges() < j2.points_prestiges()) {
+			return true;
+		}
+        return false;
+    }
+	
+	
+	
+	/**
+	 * Returns the index of the player with the least prestige points in the index interval [start, end[.
+	 * 
+	 * @param  list
+	 * 			List of players
+	 * 
+	 * @param  debut
+	 *         Integer representing the start index
+	 * 
+	 * @param  fin
+	 *         Integer representing the end index
+	 */
+	private static int indexOfMin(ArrayList <Joueur> list, int debut, int fin){
+
+        Objects.requireNonNull(list);
+
+        if(debut < 0){
+            throw new IllegalArgumentException("Debut index given under 0 (indexOfMin)");
+        }
+        if(fin < 0){
+            throw new IllegalArgumentException("End index given under 0 (indexOfMin)");
+        }
+
+        Joueur min = list.get(debut);
+        int index = debut;
+        int min_index = debut;
+
+        for(var i = debut; i < fin; i++){
+            if(compareJoueur(min, list.get(i))){
+                min = list.get(i);
+                min_index = index;
+            }
+            index ++;
+        }
+
+        return min_index;
+    }
+	
+	/**
+	 * Sorts the list of players in ascending order. The comparison between the players is done on
+	 * the number of prestige points, with equal prestige points the player with the least
+	 * development cards wins.
+	 * 
+	 * @param  classement
+	 * 	 	   List of players
+	 * 
+	 * @param  size
+	 * 		   List size
+	 */
+	private static boolean sortClassement(ArrayList<Joueur> classement, int size) {
+		
+		Objects.requireNonNull(classement);
+		
+		for(var i = 0; i < size ; i++){
+            swap(classement, i, indexOfMin(classement, i, size));     /*On va jusqu'à array.length car indexOfMin() va dans l'intervalle [debut, fin[*/
+        }
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Removes a set amount of available tokens.
+	 * 
+	 * @param jeton
+	 * 		  Type of tokens to remove
+	 * 
+	 * @param quantite
+	 * 		  Number of tokens to remove
+	 * 
+	 * @return True if it has been successfully removed or false.		
+	 *
+	 */
+	private boolean enleveRessource(Jeton jeton, int quantite) {
+		
+		int quantite_total = this.jetons_disponibles().get(jeton.couleur()) - quantite;
+		
+		if(quantite_total < 0) {
+			return false;
+		}
+		
+		this.jetons_disponibles().put(jeton.couleur(), quantite_total);
+		
+		return true;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * This method enables to know either a user succeed or not to acquire two ressources
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the purchase
+	 * @return 
+	 * an int which represents either the transaction occured properly;
+	 * 
+	 * 1 = transaction has worked
+	 * 0 = transaction canceled by the user
+	 * -1 = transaction failed
+	 */
+	private int choixDeuxJetons(Joueur joueur){
+		System.out.println("Vous avez choisi de prendre deux jetons de la même couleur, veuillez précisez leur couleur ");
+		
+		int valid_choice = 0;
+		
+		while(valid_choice != 1) {
+			var jeton = Saisie.saisieJeton();
+			
+			//Faire une fonction qui fait simultanément les deux actions
+			if(this.jetons_disponibles().get(jeton.couleur()) < 4) {
+				
+				System.out.println("\n/!\\ Il n'y a pas assez de ressources pour effectuer cette action\n");
+				
+			}else {
+				joueur.addRessource(jeton,2);
+				this.enleveRessource(jeton, 2);
+				valid_choice += 1;
+			}
+		}
+		return valid_choice;
+		
+	}
+	
+	
+	
+	/**
+	 * This method handles the whole process necessary to acquire 3 tokens
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the purchase
+	 * @return 
+	 * an int which represents either the transaction occured properly;
+	 * 
+	 * 1 = transaction has worked
+	 * 0 = transaction canceled by the user
+	 * -1 = transaction failed
+	 */
+	private int choixTroisJetons(Joueur joueur){
+		
+		Objects.requireNonNull(joueur);
+		
+		var already_choosen = new ArrayList<Jeton>();
+		boolean suite;
+		
+		System.out.println("Vous avez choisi de prendre 3 jetons différents, veuillez précisez leur couleur ");
+		System.out.println(" (en les séparant par la touche entrée) : \n");
+		
+		for(int i= 0; i < 3;i++){
+			
+			suite = false;
+			
+			while(!suite) {
+				
+				var jeton = Saisie.saisieJeton();	 //On accède à la classe Saisie pour la saisie des jetons
+
+				
+				if(!already_choosen.contains(jeton)) {
+					
+					
+					if(!this.enleveRessource(jeton, 1)) {
+							
+						System.out.println("\n/ ! \\ Pas assez de ressource pour effectuer cette action\n");
+						i --;
+						break;
+					}
+						
+					joueur.addRessource(jeton,1);
+				    already_choosen.add(jeton);
+				    suite = true;
+					
+				}else {
+					System.out.println("\n/ ! \\ Couleur déjà choisie\n");
+				}
+			}
+			  
+		}
+		return 1;
+		
+	}
+	
+	
+	
+	
+	/**
+	 * This method handles the whole process necessary to make the user obtain ressources
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the purchase
+	 * @return 
+	 * an int which represents either everything occured well or not
+	 * 
+	 * 1 = everything worked
+	 * 0 = the player has canceled his choice
+	 * -1 = a problem occured the user has to redo his turn
+	 */
+	public default int priseRessource(Joueur joueur){
+
+		AffichageLigneCommande.showJeton(this.jetons_disponibles(), "JETON");
+		
+		Objects.requireNonNull(joueur);
+		
+		var choix = Saisie.choixJeton();
+		
+		/* cas où l'utilisateur veut prendre 2 jetons identiques*/
+		if(choix == 1) {
+			var res = choixDeuxJetons(joueur);
+			return res;
+		}
+		
+		/* cas où l'utilisateur veut prendre 3 jetons différents*/
+		else if(choix == 2) {
+			var res = choixTroisJetons(joueur);
+			return res;
+		}
+		/* cas où l'utilisateur annule son action*/
+		return 0;
+	}
+	
+	
+	/**
+	 * This method check if the user as in the worst cast scenario 10 tokens
+	 * If they possess more it force him to loss some
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the checking
+	 * 
+	 */
+	public default void controleJeton(Joueur joueur) {
+		
+		Objects.requireNonNull(joueur);
+		
+		if(!joueur.checkNbJetons()) {
+			
+			while(!joueur.checkNbJetons()) {
+				
+				System.out.println("\n/!\\ Vous possèdez trop de jetons veuillez en supprimer pour en avoir 10 maximum\n");
+				AffichageLigneCommande.showJeton(joueur.ressources(), null);
+				
+				System.out.println("\nJeton");
+				
+				String jeton;
+				int quantite;
+				do{
+					jeton = Saisie.saisieJeton_name();
+					
+					System.out.println("Quantite"); 
+					
+					quantite = Saisie.nb_jeton_defausse();
+					
+				}while(Saisie.valideDefausse(joueur, jeton, quantite) == false || quantite > joueur.NbJetons_loose());
+				
+				System.out.println("\n Suppression réussie ! \n");
+				joueur.ressources().put(jeton, joueur.enleveRessource(jeton, quantite, this.jetons_disponibles()));
+				
+				this.jetons_disponibles().put(jeton, this.jetons_disponibles().get(jeton) + quantite);
+			}
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	/**
+	 * Determine who from the list of players is the winner. The winner
+	 * is the one with the most prestige points. If two players
+	 * have the same number of points, the player with the least number of cards is
+	 * cards.
+	 */
+	public default Joueur isWinner() {
+		
+		ArrayList<Joueur> classement = new ArrayList<Joueur>();
+		int nb_best_players = 0;
+		
+		
+		for(var joueur : this.joueurs()) {
+			
+			if(joueur.points_prestiges() >= VICTORY_POINTS) {
+				classement.add(joueur);
+				nb_best_players += 1;
+			}
+		}
+		
+		sortClassement(classement, nb_best_players);
+		return topClassement(classement);
+	}
+	
+	/**
+	 * Allows the users to choose the number of player they wants.
+	 * @return
+	 * the number of players choosen. 
+	 */
+	public int choixNbJoueurs();
+	
+	
+}
---
+++ ModeI.java
@@ -0,0 +1,591 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public class ModeI implements Mode {
+	/**
+	 * Number of victory points
+	 */
+	private static final int VICTORY_POINTS = 15;
+	
+	/**
+	 * Players list on the game
+	 */
+	private ArrayList<Joueur> joueurs;
+	
+	/**
+	 * Pick  of dev cards
+	 */
+	private HashMap <Integer, List<CarteDev>> pioche;
+	
+	/**
+	 * Pick size
+	 */
+	private int taille_pioche;
+	
+	/**
+	 * Board of the game
+	 */
+	private HashMap <Integer, List<CarteDev>> board;
+	
+	/**
+	 * Toeksn available
+	 */
+	private HashMap<String, Integer> jetons_disponibles;	/*On met une Map pour représenter les piles de jetons*/
+	
+	/**
+	 * Nobles cards on the board
+	 */
+	private ArrayList<Tuile> tuiles_board;
+	
+	/*
+	 * A supprimer probablement
+	 * The player for who it's the turn to play.
+	 
+	private Joueur current_player;*/
+	/*
+	 * This variable enables to know if the turn happened well.
+	 
+	private int tour_valide = 1;
+	*/
+	
+	/**
+	 * Constructor of the type Partie.
+	 */
+	public ModeI() {
+		
+		joueurs = new ArrayList<Joueur>();
+		
+		// Pioche des 4 niveaux de développement
+		pioche = new HashMap<>();
+		pioche.put(1, new ArrayList<>());
+		pioche.put(2, new ArrayList<>());
+		pioche.put(3, new ArrayList<>());
+		
+		taille_pioche = 0;
+		
+		// Board des 4 niveaux de développement
+		board = new HashMap<>();
+		board.put(1, Arrays.asList(null, null, null, null));
+		board.put(2, Arrays.asList(null, null, null, null));
+		board.put(3, Arrays.asList(null, null, null, null));
+		
+		jetons_disponibles = new HashMap<String, Integer>();
+		
+		tuiles_board = new ArrayList<>();
+	}
+	
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public ArrayList<Joueur> joueurs() {
+		return this.joueurs;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public HashMap<Integer, List<CarteDev>> board() {
+		return this.board;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public HashMap<String, Integer> jetons_disponibles() {
+		return this.jetons_disponibles;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Tuile> tuiles_board() {
+		return this.tuiles_board;
+	}
+	
+	
+	public HashMap <Integer, List<CarteDev>> pioche(){
+		return this.pioche;
+	}
+	
+	public int taille_pioche(){
+		return this.taille_pioche;
+	}
+	
+	/**
+	 * Initialize a game
+	 * 
+	 * @param mode
+	 *        Game mode
+	 */
+	public void initialisePartie(int mode) {
+		
+		this.initialiseOrdreJoueurs();
+		this.initialiseJetons();
+	
+		//Mode de jeu 1
+		if(mode == 1) {
+			
+			this.initialisePioche();
+			this.piocheFourCards(1);
+			
+		}else {
+			
+			try{
+				this.loadDeck(Path.of("src/Cartes_Devs.txt"));
+				
+			}catch(IOException e) {
+		    	System.out.println(e.getMessage());
+		    	System.exit(1);
+		    
+		    }
+			
+			this.piocheFourCards(1);
+			this.piocheFourCards(2);
+			this.piocheFourCards(3);
+			
+			this.initialiseTuiles();
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public int giveNbPlayersPossible() {
+		return 2;
+	}
+	
+	
+	/**
+	 * Load Noble card in a file.
+	 * 
+	 * 
+	 * @param path
+	 *        File path
+	 *        
+	 * @param tuiles
+	 *        Noble Cards
+	 *        
+	 * @throws IOException If there is a problem in the read of the file
+	 */
+	private void loadTuiles(Path path, ArrayList<Tuile> tuiles) throws IOException {
+		
+		Objects.requireNonNull(path, "File path given is null");
+		
+		Tuile carte;
+		
+		try(var reader = Files.newBufferedReader(path)) {  //On teste et renvoie une erreur si problème
+			 String line;
+			 while ((line = reader.readLine()) != null) {
+				 
+				 carte = Tuile.fromText(line);
+				 
+
+				 tuiles.add(carte);
+				 
+			 }
+		} // appelle reader.close()
+	}
+	
+	/**
+	 * Initialise Noble cards. There are (nb player + 1) nobles on the board
+	 */
+	private void  initialiseTuiles() {
+	
+		var all_tuiles = new ArrayList<Tuile>();
+		
+		try {
+			this.loadTuiles(Path.of("src/Tuiles.txt"), all_tuiles);
+			
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		
+		
+		for(var i = 0; i < this.joueurs.size() + 1 ;i++) {
+			
+			Collections.shuffle(all_tuiles);
+			
+			this.tuiles_board.add(all_tuiles.get(all_tuiles.size() - 1));
+			
+			all_tuiles.remove(all_tuiles.size() - 1);
+		}
+	}
+	
+	/**
+	 * Change the order of the players list to make the the younger player start.
+	 */
+	private void initialiseOrdreJoueurs() {
+		
+		int i = 0;
+		ArrayList<Joueur> extrait = new ArrayList<Joueur>();
+		ArrayList<Joueur> base = new ArrayList<Joueur>();
+		
+		int indice_younger = findYounger(this.joueurs);
+		
+		
+		for(var joueur : this.joueurs) {
+			
+			if(i < indice_younger) {
+				extrait.add(joueur);
+			}else {
+				base.add(joueur);
+			}
+			
+			i++;
+		}
+		
+		for(var joueur_extrait : extrait) {
+			base.add(joueur_extrait);
+		}
+		
+		this.joueurs = base;
+	}
+
+	/**
+	 * 
+	 * Return the index of the youngest player in the list. Returns the value -1 if there is a problem.
+	 * 
+	 * @param joueurs
+	 *        List of the players
+	 *        
+	 * @return Index of the the youngest player
+	 */
+	private static int findYounger(ArrayList<Joueur> joueurs) {
+		
+		int i = 0;
+		int indice_younger = -1;
+		
+		Joueur younger = joueurs.get(i);
+		
+		for(var joueur : joueurs) {
+			
+			if(younger.age() > joueur.age()) {
+				younger = joueur;
+				indice_younger = i;
+			}
+			
+			i++;
+		}
+		
+		return indice_younger;
+	}
+	
+	
+	/**
+	 * Initialise all the available tokens. There are 7 tokens available by color.
+	 */
+	private void initialiseJetons() {
+		
+		this.jetons_disponibles.put("Rouge", 7);	/*On mettra une constante dans la classe pour 7 jetons*/
+		this.jetons_disponibles.put("Vert", 7);
+		this.jetons_disponibles.put("Bleu", 7);
+		this.jetons_disponibles.put("Noir", 7);
+		this.jetons_disponibles.put("Blanc", 7);
+	}
+	
+	/**
+	 * Initialize the deck made up of cards in a random manner. Each card has one of the following colors
+	 * following colors: Red, Green, Black, Blue, White, Yellow, White.
+	 */
+	private void initialisePioche() {
+		
+		/*Les cartes de la pioche ne peuvent que possèder ces couleurs*/
+		var couleurs = List.<String>of("Rouge", "Vert", "Noir", "Bleu", "Blanc");
+		
+		/*Pour chaque couleur on crée 8 cartes*/
+		for(var elem : couleurs) {
+			for(int i = 0; i < 8 ;i++) {
+				
+				var map = new HashMap<String, Integer>();
+				map.put(elem, 1);
+				
+				this.pioche.get(1).add(new CarteDev(0,elem,3,null,map));
+				this.taille_pioche += 1;
+			}
+		}
+		
+		/*On mélange la pioche*/
+		Collections.shuffle(this.pioche.get(1));
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Load the development card where the informations are in a file. It makes the picks.
+	 * 
+	 * @param path
+	 *        File path
+	 *        
+	 * @throws IOException
+	 */
+	private void loadDeck(Path path) throws IOException {
+		
+		Objects.requireNonNull(path, "File path given is null");
+		
+		CarteDev carte;
+		
+		try(var reader = Files.newBufferedReader(path)) {  //On teste et renvoie une erreur si problème
+			 String line;
+			 while ((line = reader.readLine()) != null) {
+				 
+				 carte = CarteDev.fromText(line);
+				 
+
+				 this.pioche.get(carte.niveau()).add(carte);
+				 
+				 this.taille_pioche += 1;
+				 
+			 }
+		} // appelle reader.close()
+		
+		Collections.shuffle(this.pioche.get(1));
+		Collections.shuffle(this.pioche.get(2));
+		Collections.shuffle(this.pioche.get(3));
+	}
+	
+	/**
+	 * Remove the noble chosen 
+	 * 
+	 * @param noble_chosen
+	 *        Noble chosen to remove
+	 */
+	private void efface_noble(Tuile noble_chosen) {
+		
+		var iterator = this.tuiles_board.iterator();
+		while(iterator.hasNext()) {
+			var tuile1 = iterator.next();
+			if(tuile1.equals(noble_chosen)) {
+				System.out.println("Vous avez choisi : " + tuile1.name() + " ");
+				iterator.remove();
+			}
+				
+		}
+	}
+	
+	/**
+	 *  This method validate or invalidate a player's buying attempt
+	 * 
+	 * @param joueur
+	 * the player whom we will look at the reserve and the ressources
+	 * @param carte_numero
+	 * the number of the card that the user wants to buy. It allows to identify it.
+	*  @return an int which the value indicates either the operation succeed or failed
+	*  -1 = failure
+	*  1 = success
+	 */
+	private int validationAchatReserve(Joueur joueur, int carte_numero) {
+		
+		Objects.requireNonNull(joueur);
+		Objects.requireNonNull(this);
+		Objects.requireNonNull(carte_numero);
+		
+		//3 lignes
+		/* cas où il n'est pas possible d'acheter la carte, soit parce que le numéro était erroné, soit parce que l'utilisateur n'avait pas réservé de carte*/
+		/* la cause est déterminé avec un msg de la fonction précédente "achatCarteReservee" qui vient de Saisie*/
+		if(carte_numero <= 0) {
+			
+			return -1;
+		}
+		else if(joueur.acheteCarte(joueur.reserve().get(carte_numero-1), this)) {
+			var carte_delete = joueur.reserve().get(carte_numero-1);
+			joueur.reserve().remove(carte_delete);
+			System.out.println("\n Votre achat a été réalisé avec succès ! \n");
+			return 1;
+		}
+		/* cas où la carte coûte trop cher, l'utilisateur revient au menu précédent
+		 *  de force car il ne peut pas se payer cette carte*/
+			/*tour_valide = -1;*/
+		System.out.println("\nVous n'avez pas assez de ressources pour acheter cette carte !\n");
+		return -1;
+	}
+	
+	/**
+	 *  This method validate or invalidate a player's buying attempt
+	 * 
+	 * @param joueur
+	 * 		   The player whom we will look at the reserve and the ressources
+	 * 
+	 * @param carte_numero
+	 *        The numbers of the card that the user wants to buy. It allows to identify it.
+	 *        
+	*  @return An int which the value indicates either the operation succeed or failed
+	*  
+	*  -1 = failure
+	*  1 = success
+	*  0 = the player wanted to cancel
+	 */
+	private int validationAchatNonReserve(Joueur joueur, HashMap<Integer, Integer> carte ) {
+		
+		Objects.requireNonNull(joueur);
+		Objects.requireNonNull(this);
+		Objects.requireNonNull(carte);
+		
+		var niveau = carte.entrySet().stream().findFirst().get().getKey();
+		var choosen_card = carte.entrySet().stream().findFirst().get().getValue();
+		HashMap<Integer,Partie> res = new HashMap<Integer,Partie>();
+		if(choosen_card <= 3) {
+		
+			if(joueur.acheteCarte(this.board().get(niveau).get(choosen_card), this)) {
+				System.out.println("\nVotre carte a été achetée avec succès !\n");
+				this.piocheOneCard(niveau, choosen_card);	
+				return 1;
+			}
+			/* cas où la carte coûte trop cher, l'utilisateur revient au menu précédent de force car il ne peut pas se payer cette carte*/
+			else {
+				System.out.println("\nVous n'avez pas assez de ressources pour acheter cette carte !\n");
+				return -1;
+			}
+		}
+		/* l'utilisateur revient au menu précédent*/
+		else if(choosen_card == 4){
+			System.out.println("\n Action annulée !\n");
+			return 0;
+		}
+		/* cas où la carte n'existe pas, l'utilisateur revient au menu précédent de force car le numéro de carte n'existe pas*/
+		System.out.println("\n Ce numéro de carte n'existe pas !\n");
+		return 0;
+	}
+	
+	/**
+	 * This method handles the whole process necessary for a transaction
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the purchase
+	 *       
+	 * @param mode_jeu
+	 *       the game mode which had been chosen.
+	 *       
+	 * @return an int which represents either the transaction occured properly;
+	 * 
+	 * 1 = transaction has worked
+	 * 0 = transaction canceled by the user
+	 * -1 = transaction failed
+	 */
+	
+	public int achatCarte(Joueur joueur, int mode_jeu, Affichage affichage) {
+		
+		/*--------- Achat de cartes --------------*/
+		affichage.showBoard(this);
+		Objects.requireNonNull(this);
+		Objects.requireNonNull(joueur);
+		
+		if(mode_jeu != 1 && mode_jeu != 2) {
+			throw new IllegalArgumentException("mode_jeu must be 1 or 2");
+		}
+		var choix = Saisie.choixAchatCarte(this, mode_jeu, affichage);
+		/* cas où l'utilisateur achète une carte non-réservée*/
+		if(choix == 1) {
+			var carte = Saisie.achatCarteNonReservee(mode_jeu);
+			return validationAchatNonReserve(joueur, carte);
+		}
+		/* cas où l'utilisateur achète une carte réservée*/
+		var carte = Saisie.achatCarteReservee(joueur, affichage);
+		return validationAchatReserve(joueur, carte);
+	}
+	
+	/**
+	 * This method handles the whole process necessary to enable a card reservation for the user
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the rservation
+	 * @return 
+	 * an int which represents either everything occured well or not
+	 * 
+	 * 1 = everything worked
+	 * -1 = a problem occured the user has to redo his turn
+	 */
+	public int reservationCarte(Joueur joueur) {
+		
+		Objects.requireNonNull(joueur);
+		
+		var choix = Saisie.choixReservation();
+		
+		if(choix ==  1) {
+			var carte = Saisie.reservationCartePlateau();
+			return validationReservationCartePlateau(joueur,carte);
+		}
+		var carte = Saisie.reservationCartePioche();
+		return validationReservationCartePioche(joueur,carte);
+	}
+	
+	/**
+	 * This method validate or nullify the action of a card reservation made by a user
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the reservation
+	 *  @param niveau_carte
+	 *       the card that interests the user level 
+	 * @return 
+	 * an int which represents either everything occured well or not
+	 * 
+	 * 1 = everything worked
+	 * -1 = a problem occured the user has to redo his turn
+	 */
+	private int validationReservationCartePioche(Joueur joueur, int niveau_carte) {
+		
+		Objects.requireNonNull(joueur);
+		Objects.requireNonNull(niveau_carte);
+		
+		if(Saisie.carte_reserve_valide_1arg(this,niveau_carte) != 0){
+			joueur.reserveCarte(this.pioche.get(niveau_carte).get(this.pioche.size() - 1), this.jetons_disponibles);
+			return 1;
+		}
+		System.out.println("Erreur les cartes de ce niveau ne sont plus disponibles! Vous recommencez votre tour ");
+		return -1;		
+
+	}
+	
+	/**
+	 * This method handles the whole process necessary to make the user obtain ressources
+	 * 
+	 * @param joueur
+	 *       the player who his concerned by the purchase
+	 *       carte
+	 *       the card that the players want to reserve
+	 * @return 
+	 * an int which represents either everything occured well or not
+	 * 
+	 * 1 = everything worked
+	 * -1 = a problem occured the user has to redo his turn
+	 */
+	private int validationReservationCartePlateau(Joueur joueur, HashMap<Integer, Integer> carte) {
+		Objects.requireNonNull(joueur);
+		Objects.requireNonNull(carte);
+		var niveau_carte = carte.entrySet().stream().findFirst().get().getKey();
+		var num_carte =  carte.entrySet().stream().findFirst().get().getValue();;
+		if(Saisie.carte_reserve_valide_2arg(this,niveau_carte,num_carte) != 0){
+			joueur.reserveCarte(this.board.get(niveau_carte).get(num_carte), this.jetons_disponibles);
+			this.piocheOneCard(niveau_carte, num_carte);
+			return 1;
+		}
+			System.out.println("Erreur ce numéro de carte n'existe pas, vous recommencez votre tour !");
+			return -1;		
+	}	
+	
+	/**
+	 * Allows the users to choose the number of player they wants.
+	 * 
+	 * @return
+	 * the number of player choosen. In this specific mode it must be the maximum allowed by this mode, 2, there is no choice.
+	 */
+	@Override
+	public int choixNbJoueurs() {
+		return this.giveNbPlayersPossible();	
+	}
+}
