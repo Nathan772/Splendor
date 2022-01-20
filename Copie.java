1beb5a8d1a10df3aa6bae6bf64b07951d3d6261e
Nathan772 <48621421+Nathan772@users.noreply.github.com>
2022-01-20 05:46:55 +0100

    Create Copie.java

---
+++ Copie.java
@@ -0,0 +1,56 @@
+package fr.umlv.copie;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Copie {
+	
+	/**This function do a deep copy of an HashMap.
+	 * @param the hashmap that one wants to copy.
+	 * 
+	 * @return the copy which has its own references.
+	 */
+	public <T,U>HashMap<T,U> copieHashmap(HashMap<T,U> hash) {
+		Objects.requireNonNull(hash, "the hashmap argument can't be null");
+		HashMap<T,U> copie = new HashMap<T,U>();
+		if(hash.size() == 0)
+			return copie;
+		/* copie les éléments*/
+		for(HashMap.Entry<T,U> entry : hash.entrySet()){
+			var cle = entry.getKey();
+			var value = entry.getValue();
+			/* si on tombe sur une chaine on fait une copie profonde de chaque char*/
+			if(cle instanceof String cle3) {
+	/* ça semble inévitable ici de faire un cast. Nous savons que c'est un string mais le compilateur non.
+	 * Mais c'est à éviter normalement*/
+				cle =  (T) copieChaine(cle3);
+			}
+	/* ça semble inévitable ici de faire un cast. Nous savons que c'est un string mais le compilateur non.
+	* Mais c'est à éviter normalement*/
+			if(value instanceof String value3) {
+				value =  (U) copieChaine(value3);
+			}
+			copie.put(cle, value);
+		}
+		/* renvoie la copie qui a ses propres références pour les valeurs*/
+		return copie;
+	}
+	/**This function do a deep copy of a String.
+	 * @param the String that one wants to copy.
+	 * 
+	 * @return the copy which has its own references.
+	 */
+	
+	public String copieChaine(String src) {
+		var dest = new String();
+		var builder = new StringBuilder();
+		for(var i =0;i<src.length();i++) {
+			builder.append(src.charAt(i));
+		}
+		dest = builder.toString();
+		return dest;
+	}
+	
+	
+
+}
